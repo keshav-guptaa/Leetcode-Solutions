644cc05e4d1065a7ec7c59dcd638ff5cede5a686
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2022-12-12 13:09:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2=1, prev=1;
+        int curr;
+        for(int i=2; i<=n; i++){
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
