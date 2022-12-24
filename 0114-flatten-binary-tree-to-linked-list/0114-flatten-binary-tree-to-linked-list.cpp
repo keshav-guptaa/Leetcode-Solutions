553420df7a8e452973652de77d3fdc3c5a6929a1
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2022-12-24 20:08:42 +0530

    Time: 0 ms (100.00%), Space: 12.7 MB (39.69%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,13 +11,26 @@
  */
 class Solution {
 public:
-    TreeNode* prev = NULL;
+    //TreeNode* prev = NULL;
     void flatten(TreeNode* root) {
+        /*
         if(root == NULL) return;
         flatten(root -> right);
         flatten(root -> left);
         root -> right = prev;
         root -> left = NULL;
-        prev = root;
+        prev = root; //whose reverse postorder is complete is alloted to                      prev.
+        */
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr -> left != NULL){
+                TreeNode* prev = curr -> left;
+                while(prev -> right) prev = prev -> right;
+                prev -> right = curr -> right;
+                curr -> right = curr -> left;
+                curr -> left = NULL;
+            }
+            curr = curr -> right;
+        }
     }
 };
