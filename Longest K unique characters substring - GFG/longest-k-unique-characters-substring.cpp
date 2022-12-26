e42d1a272732db4539182e095168dd949da8ffbd
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2022-12-26 19:27:56 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
@@ -16,9 +16,11 @@ class Solution{
             m[s[j]]++;
             if(m.size() == k) ans = max(ans, j-i+1);
             else if(m.size() > k){
-                m[s[i]]--;
-                if(m[s[i]] == 0) m.erase(s[i]);
-                i++;
+                while(m.size() > k){
+                    m[s[i]]--;
+                    if(m[s[i]] == 0) m.erase(s[i]);
+                    i++;
+                }
             }
             j++;
         }
