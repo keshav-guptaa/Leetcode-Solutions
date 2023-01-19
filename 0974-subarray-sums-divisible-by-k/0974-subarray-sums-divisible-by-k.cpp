dcd3a9c782f66552e4d10a5df72c54d2a5b83a06
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-01-19 16:03:30 +0530

    Time: 74 ms (56.60%), Space: 31.9 MB (19.32%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -8,16 +8,11 @@ public:
         map<int, int> m;
         int ans = 0;
         for(int i = 0; i < n; i++){
-            //cout << nums[i] << " ";
-            
             nums[i] %= k;
             if(nums[i] < 0){
                 nums[i] += k;
             }
-            //cout << nums[i] << " ";
-            ans += m[nums[i]];
-            //cout << ans << endl;
-            m[nums[i]]++;
+            ans += m[nums[i]]++;
         }
         ans += m[0];
         return ans;
