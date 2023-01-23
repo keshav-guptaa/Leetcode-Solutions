52166c1c880bdbfebe04438a08c44d436b597332
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-01-23 20:34:26 +0530

    Time: 434 ms (15.77%), Space: 58.7 MB (44.89%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int> &vis, vector<int> &pathvis, vector<vector<int>>& graph){
-        vis[node] = 1;
-        pathvis[node] = 1;
-        //check[node] = 0;
-        
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                if(dfs(it, vis, pathvis, graph)) return true;
-            }
-            else if(pathvis[it]){
-                return true;
-            }
-        }
-        pathvis[node] = 0;
-        //check[node] = 1;
-        return false;
-    }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n, 0);
-        vector<int> pathvis(n, 0);
-        //vector<int> check(n, 0);
-        vector<int> safeNodes;
+        vector<int> adj[n];
+        vector<int> in(n, 0);
         for(int i = 0; i < n; i++){
-            if(!vis[i]) dfs(i, vis, pathvis, graph);
+            for(auto it: graph[i]){
+                adj[it].push_back(i);
+                in[i]++;
+            }
         }
+        vector<int> ans;
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(!in[i]) q.push(i);
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: adj[node]){
+                in[it]--;
+                if(!in[it]) q.push(it);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
         
-        for(int i = 0; i < n; i++){
-            if(!pathvis[i]) safeNodes.push_back(i);
-        }
-        return safeNodes;
     }
 };
