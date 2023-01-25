e576607e3fca0843abb414e34e9e7f56e11663b8
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-01-26 01:15:26 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
@@ -8,41 +8,37 @@ class Solution {
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
         // Code here
         vector<pair<int, int>> adj[n+1];
-        for(auto it: edges){
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
+        for(int i = 0; i < edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
             adj[u].push_back({v, wt});
             adj[v].push_back({u, wt});
         }
         
-        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-        vector<int> dist(n + 1, 1e9);
-        vector<int> parent(n + 1);
-        for(int i = 0; i <= n; i++) parent[i] = i;
-        
-        pq.push({0, 1});
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> dist(n+1, 1e9), parent(n+1);
+        for(int i = 0; i < n; i++) parent[i] = i;
         dist[1] = 0;
+        pq.push({0, 1});
         while(!pq.empty()){
-            int node = pq.top().second;
             int d = pq.top().first;
+            int u = pq.top().second;
             pq.pop();
             
-            for(auto it: adj[node]){
-                int adjNode = it.first;
+            for(auto it: adj[u]){
+                int v = it.first;
                 int wt = it.second;
-                
-                if(wt + d < dist[adjNode]){
-                    dist[adjNode] = wt + d;
-                    parent[adjNode] = node;
-                    pq.push({dist[adjNode], adjNode});
+                if(d + wt < dist[v]){
+                    dist[v] = d + wt;
+                    pq.push({dist[v], v});
+                    parent[v] = u;
                 }
             }
         }
-        
+        int node = n;
         if(dist[n] == 1e9) return {-1};
         vector<int> ans;
-        int node = n;
         while(parent[node] != node){
             ans.push_back(node);
             node = parent[node];
