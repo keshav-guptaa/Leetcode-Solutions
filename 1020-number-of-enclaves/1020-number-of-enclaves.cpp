3a1a39f842afb9f0cb9a95b95961b196c8151a3a
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-01-26 10:55:57 +0530

    Time: 100 ms (40.31%), Space: 24.9 MB (98.90%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
         queue<pair<int, int>> q;
         
         for(int i = 0; i < n; i++){
             if(grid[i][0] == 1){
                 q.push({i, 0});
-                vis[i][0] = 1;
+                grid[i][0] = 0;
             } 
             if(grid[i][m-1] == 1){
                 q.push({i, m-1});
-                vis[i][m-1] = 1;
+                grid[i][m-1] = 0;
             } 
         }
         
         for(int j = 0; j < m; j++){
             if(grid[0][j] == 1){
                 q.push({0, j});
-                vis[0][j] = 1;
+                grid[0][j] = 0;
             } 
             if(grid[n-1][j] == 1){
                 q.push({n-1, j});
-                vis[n-1][j] = 1;
+                grid[n-1][j] = 0;
             } 
         }
         
@@ -38,18 +36,19 @@ public:
             for(int i = 0; i < 4; i++){
                 int nrow = delrow[i] + row;
                 int ncol = delcol[i] + col;
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1){
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1){
                     q.push({nrow, ncol});
-                    vis[nrow][ncol] = 1;
+                    grid[nrow][ncol] = 0;
                 }
             }
         }
         int ans = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0) ans++;
+                if(grid[i][j] == 1) ans++;
             }
         }
         return ans;
+        
     }
 };
