ad42499f77dc8e92e4f8912a1f5b979cb5ef9a05
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-01-28 11:07:25 +0530

    Time: 81 ms (61.28%), Space: 21.7 MB (54.83%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,33 +3,34 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         vector<vector<int>> vis(n, vector<int>(n, 0));
-        if(grid[0][0] == 1) return -1;
-        queue< pair< int, pair<int, int> > > q;
-        q.push({1, {0, 0}});
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        queue<pair<pair<int, int>, int>> q;
+        q.push({{0, 0}, 1});
         vis[0][0] = 1;
+        
+        
         while(!q.empty()){
-            //cout << 1 << endl;
             auto it = q.front();
             q.pop();
-            int row = it.second.first;
-            int col = it.second.second;
-            int d = it.first;
-            if(row == n-1 && col == n-1){
-                return d;
-            }
+            int row = it.first.first;
+            int col = it.first.second;
+            int d = it.second;
+            if(row == n-1 && col == n-1) return d;
             
             for(int i = -1; i <= 1; i++){
                 for(int j = -1; j <= 1; j++){
                     int nrow = row + i;
                     int ncol = col + j;
-                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 0){
+                    
+                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n 
+                       && grid[nrow][ncol] == 0 && vis[nrow][ncol] == 0){
                         vis[nrow][ncol] = 1;
-                        q.push({d+1, {nrow, ncol}});
+                        q.push({{nrow, ncol}, d+1});
                     }
                 }
             }
         }
-        return -1;
         
+        return -1;
     }
 };
