5e7fefd2140df4cfd18067747e7aad04b0b9ef5e
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-03-23 13:04:16 +0530

    Time: 21 ms (32.55%), Space: 8.9 MB (68.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -12,11 +12,26 @@ public:
         }
     }
     
-    int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return memoization(n-1, m-1, word1, word2, dp);
+    int minDistance(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+                }
+            }
+        }
+        
+        return dp[n][m];
         
     }
 };
