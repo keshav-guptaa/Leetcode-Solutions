8b044a4362cfcc42673802bd6bd9ae74d26874e7
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-03-23 13:05:07 +0530

    Time: 22 ms (30.53%), Space: 9.1 MB (19.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    int memoization(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s[i] == t[j]) return dp[i][j] = memoization(i-1, j-1, s, t, dp);
-        else{
-            return dp[i][j] = 1 + min(memoization(i-1, j-1, s, t, dp), min(memoization(i-1, j, s, t, dp), memoization(i, j-1, s, t, dp)));
-        }
-    }
-    
     int minDistance(string s, string t) {
         int n = s.length();
         int m = t.length();
