be5cf75515463130dc7901b3ba5fa1f52ffaa374
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-03-23 17:35:20 +0530

    Time: 1061 ms (43.48%), Space: 253.4 MB (19.93%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -10,11 +10,11 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 unordered_map<int, int> counter;
-                counter = {{0,1}};
+                counter[0] = 1;
                 int cur = 0;
                 for (int k = 0; k < m; k++) {
                     cur += A[k][j] - (i > 0 ? A[k][i - 1] : 0);
-                    res += counter.find(cur - target) != counter.end() ? counter[cur - target] : 0;
+                    res += counter[cur - target];
                     counter[cur]++;
                 }
             }
