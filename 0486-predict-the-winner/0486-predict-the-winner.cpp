68f9450834b4417cda6a70476a8fc5933479df08
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-03-29 11:57:36 +0530

    Time: 3 ms (72.14%), Space: 7.8 MB (23.66%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     int f(int idx1, int idx2, vector<int>& nums){
         if(idx1 > idx2) return 0;
         if(idx1 == idx2) return nums[idx1];
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
         
         int left = nums[idx1] + min(f(idx1+2, idx2, nums), f(idx1+1, idx2-1, nums));
         int right = nums[idx2] + min(f(idx1+1, idx2-1, nums), f(idx1, idx2-2, nums));
-        return max(left, right);
+        return dp[idx1][idx2] = max(left, right);
     }
     
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        dp.resize(n, vector<int>(n, -1));
         int sum = 0;
         for(int i = 0; i < n; i++) sum += nums[i];
         int x = f(0, n-1, nums);
