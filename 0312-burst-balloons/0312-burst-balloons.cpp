c6a4db11b8fe2a4e97c9e8623a2c732b35e12226
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-04-01 12:46:26 +0530

    Time: 208 ms (98.33%), Space: 8.3 MB (93.36%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -15,9 +15,23 @@ public:
     }
     
     int maxCoins(vector<int>& nums) {
+        int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.insert(nums.end(), 1);
-        memset(dp, -1, sizeof(dp));
-        return f(1, nums.size()-2, nums);
+        memset(dp, 0, sizeof(dp));
+        int ans = INT_MIN;
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j) continue;
+                int ans = INT_MIN;
+                for(int k = i; k <= j; k++){
+                    int x = nums[k]*nums[i-1]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    ans = max(ans, x);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        
+        return dp[1][n];
     }
 };
