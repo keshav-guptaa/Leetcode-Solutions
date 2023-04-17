fee4ce7cc49380365528c612860eec26c5bb6b47
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-04-17 14:38:22 +0530

    Time: 24 ms (54.59%), Space: 8.9 MB (74.84%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> time(n, vector<int>(n, 1e9));
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        pq.push({grid[0][0], 0, 0});
+        time[0][0] = grid[0][0];
+        int dr[] = {-1, 0, 0, 1};
+        int dc[] = {0, 1, -1, 0};
+        
+        int x = 0;
+        while(!pq.empty()){
+            auto [t, r, c] = pq.top();
+            pq.pop();
+            if(r == n-1 && c == n-1){
+                x = t;
+                break;
+            }
+            
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i], nc = c + dc[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
+                    if(max(grid[nr][nc], t) < time[nr][nc]){
+                        time[nr][nc] = max(grid[nr][nc], t);
+                        pq.push({time[nr][nc], nr, nc});
+                    }
+                }
+            }
+        }
+        return x;
+    
+    }
+};
