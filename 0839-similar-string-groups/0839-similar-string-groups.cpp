e2d0d4fd755dbaf12190196b09a7c7524736d144
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-04-28 18:07:17 +0530

    Time: 170 ms (49.52%), Space: 10.2 MB (58.73%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isSimilar(string &s1, string &s2){
+        int n = s1.length();
+        int diff = 0;
+        for(int i = 0; i < n; i++){
+            if(s1[i] != s2[i]) diff++;
+        }
+        return diff == 0 || diff == 2;
+    }
+    
+    void dfs(int node, vector<int> adj[], vector<int> &vis){
+        vis[node] = 1;
+        
+        for(auto &x: adj[node]){
+            if(!vis[x]) dfs(x, adj, vis);
+        }
+    }
+    
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        vector<int> adj[n];
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(isSimilar(strs[i], strs[j])){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        vector<int> vis(n, 0);
+        int ct = 0;
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                ct++;
+                dfs(i, adj, vis);
+            }
+        }
+        return ct;   
+    }
+};
