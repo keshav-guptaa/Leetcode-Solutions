7a2ba981ebf6956ed8d171edf54b56ba6b2cab63
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-05-01 19:37:57 +0530

    Time: 549 ms (72.43%), Space: 138.9 MB (89.71%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -23,15 +23,9 @@ class DSU {
     void unite(int u, int v) {
         int p_u = find(u); 
         int p_v = find(v); 
-        if(p_u == p_v) return; 
-        if(size[p_u] < size[p_v]) {
-            parent[p_u] = p_v; 
-            size[p_v] += size[p_u]; 
-        }
-        else {
-            parent[p_v] = p_u;
-            size[p_u] += size[p_v]; 
-        }
+        if(size[p_v] > size[p_u]) swap(p_u, p_v);
+        parent[p_v] = p_u;
+        size[p_u] += size[p_v]; 
     }
 };
 
