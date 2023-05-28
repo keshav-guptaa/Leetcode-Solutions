e531ae3afd641dffd0443884a5b51ae39e292233
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-05-28 12:43:32 +0530

    Time: 36 ms (45.52%), Space: 22.5 MB (20.52%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* Solution 1
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int n =mat.size(), m = mat[0].size();
         vector<int> ans;
@@ -14,5 +15,21 @@ public:
             }
         }
         return ans;
-    }
+        */
+        vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+            int n =mat.size(), m = mat[0].size();
+            vector<vector<int>> v(n+m-1);
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    v[i+j].push_back(mat[i][j]);
+                }
+            }
+            vector<int> ans;
+            for(int i = 0; i < n+m-1; i++){
+                if(!(i&1)) reverse(v[i].begin(), v[i].end());
+                for(auto &x: v[i]) ans.push_back(x);
+            }
+            return ans;
+        }
+    
 };
