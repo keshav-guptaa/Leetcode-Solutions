eb9c3f5bb85eefca8f7e4dbd6adbec935d5d2a78
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-14 18:10:33 +0530

    Time: 8 ms (64.10%), Space: 8.9 MB (74.66%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int M = dungeon.size();
-        int N = dungeon[0].size();
-        // hp[i][j] represents the min hp needed at position (i, j)
-        // Add dummy row and column at bottom and right side
-        vector<vector<int> > hp(M + 1, vector<int>(N + 1, INT_MAX));
-        hp[M][N - 1] = 1;
-        hp[M - 1][N] = 1;
-        for (int i = M - 1; i >= 0; i--) {
-            for (int j = N - 1; j >= 0; j--) {
-                int need = min(hp[i + 1][j], hp[i][j + 1]) - dungeon[i][j];
-                hp[i][j] = need <= 0 ? 1 : need;
-            }
-        }
-        return hp[0][0];
+    int f(int r, int c, vector<vector<int>>& dp, vector<vector<int>>& grid, int n, int m){
+        if(r >= n || c >= m) return 1e9;
+        if(r == n-1 && c == m-1){
+            return grid[r][c] >= 0 ? 1 : 1-grid[r][c];  
+        } 
+        if(dp[r][c] != -1) return dp[r][c];
+        
+        int right = f(r+1, c, dp, grid, n, m);
+        int down = f(r, c+1, dp, grid, n, m);
+        int health = min(right, down) - grid[r][c];
+        return dp[r][c] = health <= 0 ? 1 : health;
+    }
+    
+    int calculateMinimumHP(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(0, 0, dp, grid, n, m);
     }
 };
