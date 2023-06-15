b49e3d69e19b1b2bcd2caf352eadc0c2ad77671b
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-15 11:37:01 +0530

    Time: 127 ms (16.92%), Space: 21.2 MB (10.34%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
+    int f(int r, int c, vector<vector<int>>& dp, vector<vector<char>>& mat, int n, int m){
+        if(r >= n || c >= m) return 0;
+        if(mat[r][c] == '0') return 0;
+        if(dp[r][c] != -1) return dp[r][c];
+        return dp[r][c] = min({f(r+1, c, dp, mat, n, m), f(r+1, c+1, dp, mat, n, m), 
+                               f(r, c+1, dp, mat, n, m)}) + 1;
+    }
+    
     int maximalSquare(vector<vector<char>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        int mx = 0;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int ans = 0;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(i == 0 || j == 0){
-                    if(mat[i][j] == '1') dp[i][j] = 1;
-                }
-                else if(mat[i][j] == '1'){
-                    dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
-                }
-                mx = max(mx, dp[i][j]);
+            for(int j = 0; j <  m; j++){
+                ans = max(ans, f(i, j, dp, mat, n, m));
             }
         }
-        return mx*mx;
+        return ans*ans;
     }
 };
