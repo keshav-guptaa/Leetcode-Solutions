faa0efb79a2e3cd965b2f7e931cb3bf110334996
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-16 14:28:01 +0530

    Time: 10 ms (32.31%), Space: 7 MB (47.51%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -5,15 +5,21 @@ public:
         int n = s.length(), m = p.length();
         vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
         dp[0][0] = true;
+        for(int j = 2; j <= m; j+=2){
+            if(p[j-1] == '*'){
+                dp[0][j-1] = 1;
+                dp[0][j] = 1;
+            }
+            else break;
+        }
         
-        
-        for(int i = 0; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 
-                    if((i && s[i-1] == p[j-1] )|| p[j-1] == '.') dp[i][j] = i && dp[i-1][j-1];
+                    if(s[i-1] == p[j-1] || p[j-1] == '.') dp[i][j] = dp[i-1][j-1];
                     else if(p[j-1] == '*'){
-                        if(( i && p[j-2] == s[i-1]) || p[j-2] == '.'){
-                            dp[i][j] = dp[i][j-2] || (i && dp[i-1][j]);
+                        if(p[j-2] == s[i-1] || p[j-2] == '.'){
+                            dp[i][j] = dp[i][j-2] || dp[i-1][j];
                         }  
                         else dp[i][j] = dp[i][j-2];
                     } 
