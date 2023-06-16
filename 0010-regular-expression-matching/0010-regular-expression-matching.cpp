e4bf905d710bd6326e83af2ce1cd758a65682788
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-16 14:26:23 +0530

    Time: 5 ms (63.11%), Space: 6.8 MB (63.55%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    bool f(int i, int j, string s, string p){
-        if(i < 0 && j < 0) return true;
-        if(j < 0 && i >= 0) return false;
-        if(i < 0 && j >= 0){
-            
-        }
-        
-        if(s[i] == p[j] || p[j] == '.'){
-            return f(i-1, j-1, s, p);
-        }
-        else{
-            if(p[j] == '*'){
-                return f(i, j-1, s, p) || f(i-1, j, s, p);
-            }
-            else return false;
-        }
-        
-    }
     
     bool isMatch(string s, string p) {
         int n = s.length(), m = p.length();
@@ -28,13 +10,14 @@ public:
         for(int i = 0; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 
-                if(p[j-1] == '*'){
-                    dp[i][j] = dp[i][j-2] || (i > 0 && dp[i-1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
-                }
-                else{
-                    dp[i][j] = i > 0 && dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
-                }
-        
+                    if((i && s[i-1] == p[j-1] )|| p[j-1] == '.') dp[i][j] = i && dp[i-1][j-1];
+                    else if(p[j-1] == '*'){
+                        if(( i && p[j-2] == s[i-1]) || p[j-2] == '.'){
+                            dp[i][j] = dp[i][j-2] || (i && dp[i-1][j]);
+                        }  
+                        else dp[i][j] = dp[i][j-2];
+                    } 
+                
             }
         }
         return dp[n][m];
