133e2cc6a742033d07de0d90a7a955da11986490
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-16 19:56:53 +0530

    Time: 27 ms (32.89%), Space: 24.8 MB (28.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int dp[102][102];
+    int m, n, N;
+    int t[101][101];
     
-    bool f(int i, int j, int k, string& s1, string& s2, string& s3){
-        if(i < 0 && j < 0 && k < 0) return true;
-        if(i >= 0 && j >= 0 && dp[i][j] != -1) return dp[i][j];
+    bool check(string s1, string s2, string s3, int i, int j) {
+        if(i >= m && j >= n && i+j >= N) return true;
+        if(t[i][j] != -1) return t[i][j];
         
-        if(i >= 0 && s1[i] == s3[k] && j >= 0 && s2[j] == s3[k]){
-            return f(i-1, j, k-1, s1, s2, s3) || f(i, j-1, k-1, s1, s2, s3);
-        }
-        else if(i >= 0 && s1[i] == s3[k]){
-            return f(i-1, j, k-1, s1, s2, s3);
-        }
-        else if(j >= 0 && s2[j] == s3[k]){
-            return f(i, j-1, k-1, s1, s2, s3);
-        }
-        
-        return false;
+        //Note that, k is same for both calls because it increases in both cases
+        //Also, at any time k = i + j;
+        return t[i][j] = ((s1[i]==s3[i+j] && check(s1, s2, s3, i+1, j)) || (s2[j]==s3[i+j] && 
+                           check(s1, s2, s3, i, j+1)));
     }
-    
     bool isInterleave(string s1, string s2, string s3) {
-        if(s3.length() != s1.length() + s2.length()) return false;
-        memset(dp, -1, sizeof(dp));
-        return f(s1.length()-1, s2.length()-1, s3.length()-1, s1, s2, s3);
+        memset(t, -1, sizeof(t));
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
+        
+        if(m + n != N)
+            return false;
+        
+        return check(s1, s2, s3, 0, 0);
     }
 };
