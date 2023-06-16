0cbc5c59a51c66fa4eec933d5b5d0d7ee2675b6f
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-16 19:23:43 +0530

    Time: 7 ms (13.94%), Space: 7.7 MB (12.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> dp;
     int f(string s, int idx){
         if(idx == s.length()) return 1;
+        if(s[idx] == '0') dp[idx] = 0;
         if(dp[idx] != -1) return dp[idx];
-        int ways = 0;
-        if(s[idx] != '0') ways += f(s, idx+1);
+        int ways = f(s, idx+1);
         if(idx+1 < s.length() && (s[idx] == '1' || (s[idx] == '2' &&
            s[idx+1] <= '6'))){
             ways += f(s, idx+2);
@@ -14,18 +14,7 @@ public:
     }
     int numDecodings(string s) {
         int n = s.length();
-        dp.resize(n+1, 0);
-        dp[n] = 1;
-        
-        for(int idx = n-1; idx >= 0; idx--){
-            int ways = 0;
-            if(s[idx] != '0') ways += dp[idx+1];
-            if(idx+1 < s.length() && (s[idx] == '1' || (s[idx] == '2' && s[idx+1] <= '6'))){
-                ways += dp[idx+2];
-            }
-            dp[idx] = ways;
-        }
-        
-        return dp[0];
+        dp.resize(n, -1);
+        return f(s, 0);
     }
 };
