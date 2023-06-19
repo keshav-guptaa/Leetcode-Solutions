abd40aa5d3def15c3ec4282339e7f69a3446e3bb
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-19 20:30:02 +0530

    Time: 66 ms (33.48%), Space: 17.1 MB (12.68%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[1005][1005][2];
+    
+    int f(int idx, int m, int id, vector<int>& p){
+        if(idx >= p.size()) return 0;
+        if(dp[idx][m][id] != -1) return dp[idx][m][id];
+        
+        int score = id ? INT_MIN : INT_MAX;
+        int sum = 0;
+        for(int i = idx; i < p.size() && i < idx + 2*m; i++){
+            sum += p[i];
+            int next = f(i+1, max(m, i-idx+1), 1-id, p);
+            if(id) score = max(score, sum + next);
+            else score = min(score, next);
+        }
+        return dp[idx][m][id] = score;
+    }
+    
+    int stoneGameII(vector<int>& p) {
+        memset(dp, -1, sizeof dp);
+        return f(0, 1, 1, p);
+        
+    }
+};
