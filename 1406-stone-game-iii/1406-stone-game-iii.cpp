aa48cc729c6fb7b8f7a8e64f3e11606b70db9313
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-19 20:44:26 +0530

    Time: 324 ms (27.90%), Space: 133 MB (47.12%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -8,49 +8,42 @@ public:
         
         int score = id ? INT_MIN : INT_MAX;
         int sum = 0;
-        if(id){
-            for(int i = 0; i <= 2; i++){
-                if(idx+i < p.size()) sum += p[idx+i];
-                score = max(score, sum + f(idx+i+1, 1-id, p));
-            }
-        }
-        else{
-            for(int i = 0; i <= 2; i++){
-                if(idx+i < p.size()) sum += p[idx+i];
-                score = min(score, -sum + f(idx+i+1, 1-id, p));
-            }
+        for(int i = idx; i < p.size() && i <= idx+2; i++){
+            sum += p[i];
+            int next = f(i+1, 1-id, p);
+            if(id) score = max(score, sum + next);
+            else score = min(score, -sum + next);
         }
         return dp[idx][id] = score;
     }
     
     string stoneGameIII(vector<int>& p) {
         int n = p.size();
-        //vector<vector<int>> dp(n, vector<int>(2, -1));
-        //memset(dp, -1, sizeof dp);
-        memset(dp, 0, sizeof dp);
+        memset(dp, -1, sizeof dp);
+        //memset(dp, 0, sizeof dp);
         
-        for(int idx = n-1; idx >= 0; idx--){
-            for(int id = 0; id <= 1; id++){
-                int score = id ? INT_MIN : INT_MAX;
-                int sum = 0;
-                if(id){
-                    for(int i = 0; i <= 2; i++){
-                        if(idx+i < p.size()) sum += p[idx+i];
-                        score = max(score, sum + dp[idx+i+1][1-id]);
-                    }
-                }
-                else{
-                    for(int i = 0; i <= 2; i++){
-                        if(idx+i < p.size()) sum += p[idx+i];
-                        score = min(score, -sum + dp[idx+i+1][1-id]);
-                    }
-                }
-                dp[idx][id] = score;
-            }
-        }
+        // for(int idx = n-1; idx >= 0; idx--){
+        //     for(int id = 0; id <= 1; id++){
+        //         int score = id ? INT_MIN : INT_MAX;
+        //         int sum = 0;
+        //         if(id){
+        //             for(int i = 0; i <= 2; i++){
+        //                 if(idx+i < p.size()) sum += p[idx+i];
+        //                 score = max(score, sum + dp[idx+i+1][1-id]);
+        //             }
+        //         }
+        //         else{
+        //             for(int i = 0; i <= 2; i++){
+        //                 if(idx+i < p.size()) sum += p[idx+i];
+        //                 score = min(score, -sum + dp[idx+i+1][1-id]);
+        //             }
+        //         }
+        //         dp[idx][id] = score;
+        //     }
+        // }
         
-        
-        int score = dp[0][1];
+        int score = f(0, 1, p);
+        //int score = dp[0][1];
         if (score > 0) return "Alice";
         if (score < 0) return "Bob";
         return "Tie";
