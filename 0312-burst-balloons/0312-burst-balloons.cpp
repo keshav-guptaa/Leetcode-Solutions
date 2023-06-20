882c7cd7f93dd11e5445e24a249b4421a8ef742c
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-20 19:04:32 +0530

    Time: 298 ms (93.82%), Space: 8.3 MB (93.82%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -8,30 +8,32 @@ public:
         
         int ans = INT_MIN;
         for(int k = i; k <= j; k++){
-            int x = nums[k]*nums[i-1]*nums[j+1] + f(i, k-1, nums) + f(k+1, j, nums);
+            int x = nums[i-1]*nums[k]*nums[j+1] + f(i, k-1, nums) + f(k+1, j, nums);
             ans = max(ans, x);
         }
         return dp[i][j] = ans;
-    }
+    } 
+    
     
     int maxCoins(vector<int>& nums) {
-        int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.insert(nums.end(), 1);
-        memset(dp, 0, sizeof(dp));
-        int ans = INT_MIN;
-        for(int i = n; i >= 1; i--){
-            for(int j = 1; j <= n; j++){
-                if(i > j) continue;
-                int ans = INT_MIN;
-                for(int k = i; k <= j; k++){
-                    int x = nums[k]*nums[i-1]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
-                    ans = max(ans, x);
-                }
-                dp[i][j] = ans;
-            }
-        }
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
         
-        return dp[1][n];
+
+        // for(int i = n; i >= 1; i--){
+        //     for(int j = 1; j <= n; j++){
+        //         if(i > j) continue;
+        //         int ans = INT_MIN;
+        //         for(int k = i; k <= j; k++){
+        //             int x = nums[k]*nums[i-1]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+        //             ans = max(ans, x);
+        //         }
+        //         dp[i][j] = ans;
+        //     }
+        // }
+        
+        return f(1, n-2, nums);
     }
 };
