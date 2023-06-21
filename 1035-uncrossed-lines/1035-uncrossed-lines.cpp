10fc2fcd1d3ccbc3a2f21a50a722cd627295a01f
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-22 00:45:58 +0530

    Time: 37 ms (23.07%), Space: 12.8 MB (60.40%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int dfs(vector<int>& A, vector<int>& B, int i, int j, vector<vector<int>> &dp) {
-  if (i >= A.size() || j >= B.size()) return 0;
-  if (dp[i][j] != -1) return dp[i][j];
-  auto nj = j;
-  while (nj < B.size() && B[nj] != A[i]) ++nj;
-  return dp[i][j] = max(dfs(A, B, i + 1, j, dp), (nj < B.size() ? 1 : 0) + dfs(A, B, i + 1, nj + 1, dp));
-}
-int maxUncrossedLines(vector<int>& A, vector<int>& B) {
-  vector<vector<int>> dp(A.size(), vector<int>(B.size(), -1));
-  return dfs(A, B, 0, 0, dp);
-}
-};
+        if (i >= A.size() || j >= B.size()) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        int ways = max(dfs(A, B, i+1, j, dp), dfs(A, B, i, j+1, dp));
+        if(A[i] == B[j]) ways = max(ways, 1 + dfs(A, B, i+1, j+1, dp));
+        return dp[i][j] = ways;
+    }
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        vector<vector<int>> dp(A.size(), vector<int>(B.size(), -1));
+        return dfs(A, B, 0, 0, dp);
+    }
+};  
