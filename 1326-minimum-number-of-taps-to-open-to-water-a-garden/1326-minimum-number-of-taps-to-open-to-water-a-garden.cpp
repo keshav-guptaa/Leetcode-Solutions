94932608191b3948d1437103c7c31a199d64f81e
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-24 18:13:10 +0530

    Time: 16 ms (87.84%), Space: 14.7 MB (54.82%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int> jumps(n+1, 0);
+        
+        for(int i = 0; i <= n; i++){
+            int l = max(0, i-ranges[i]);
+            int r = min(n, i+ranges[i]);
+            jumps[l] = max(jumps[l], r-l); //from l, that's farthest I can jump to right
+        }
+        
+        int ct = 0, currReach = 0, maxReach = 0;
+        for(int i = 0; i < n; i++){
+            maxReach = max(maxReach, i+jumps[i]);
+            
+            if(i == currReach){
+                currReach = maxReach;
+                ct++;
+            }
+        }
+        
+        return currReach >= n ? ct: -1;
+    }
+};
