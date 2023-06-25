8402e604907314d0f0829dc96e1487b488cb6df6
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-06-25 14:12:10 +0530

    Time: 223 ms (41.99%), Space: 12.1 MB (19.34%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    const int mod = 1e9+7;
+    
+    int f(int city, int fuel, int finish, vector<int>& l, vector<vector<int>>& dp){
+        if(fuel < 0) return 0;
+        if(dp[city][fuel] != -1) return dp[city][fuel];
+        
+        long long ways = city == finish ? 1: 0;
+        for(int i = 0; i < l.size(); i++){
+            if(i != city && abs(l[i]-l[city]) <= fuel){
+                ways += f(i, fuel-abs(l[i]-l[city]), finish, l, dp);
+                ways %= mod;
+            }
+        }
+        return dp[city][fuel] = ways;
+    }
+    
+    int countRoutes(vector<int>& l, int start, int finish, int fuel) {
+        int n = l.size();
+        vector<vector<int>> dp(n, vector<int>(fuel+1, -1));
+        return f(start, fuel, finish, l, dp);
+    }
+};
