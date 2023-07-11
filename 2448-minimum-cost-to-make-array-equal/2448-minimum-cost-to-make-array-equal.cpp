4b298a1cc2ee04bd42ef8f7ecb2d3a9f786f79f7
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-07-11 10:30:15 +0530

    Time: 111 ms (46.17%), Space: 38.4 MB (95.58%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -11,11 +11,11 @@ public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
         int s = 0, e = 1e6, mid;
         long long ans = f(1, nums, cost);
-        while(s < e){
+        while(s <= e){
             mid = s + (e-s)/2;
             long long x1 = f(mid, nums, cost), x2 = f(mid+1, nums, cost);
             ans = min(x1, x2);
-            if(x1 < x2) e = mid;
+            if(x1 < x2) e = mid-1;
             else s = mid+1;
         }
         return ans;
