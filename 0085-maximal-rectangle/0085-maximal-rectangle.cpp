1f82b1a8c753bc66bb02dffddf628c5b7190e11a
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-07-11 20:25:21 +0530

    Time: 59 ms (51.58%), Space: 15.9 MB (44.48%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int largestRectangleArea(vector<int>& h){
+        int n = h.size();
+        vector<int> l(n), r(n);
+        stack<pair<int, int>> st1, st2;
+        for(int i = 0; i < n; i++){
+            while(!st1.empty() && st1.top().first >= h[i]) st1.pop();
+            if(st1.empty()) l[i] = i;
+            else l[i] = i-st1.top().second-1;
+            st1.push({h[i], i});
+        }
+        for(int i = n-1; i >= 0; i--){
+            while(!st2.empty() && st2.top().first >= h[i]) st2.pop();
+            if(st2.empty()) r[i] = n-i-1;
+            else r[i] = st2.top().second-i-1;
+            st2.push({h[i], i});
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans = max(ans, (l[i]+r[i]+1)*h[i]);
+        }
+        return ans;
+    }
+    
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        vector<int> h(m, 0);
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                h[j] = mat[i][j] == '0' ? 0 : h[j]+1;
+            }
+            ans = max(ans, largestRectangleArea(h));
+        }
+        return ans;
+    }
+};
