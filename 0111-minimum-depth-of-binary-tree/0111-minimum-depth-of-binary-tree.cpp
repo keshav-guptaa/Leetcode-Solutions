46d4aae6a924f3680e3a3c7fc78546549a3e916e
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-07-13 16:32:53 +0530

    Time: 279 ms (80.94%), Space: 144.8 MB (33.84%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -14,8 +14,10 @@ public:
     
     int minDepth(TreeNode* root) {
         if(!root) return 0;
-        if(!root->left) return 1 + minDepth(root->right);
-        if(!root->right) return 1 + minDepth(root->left);
-        return 1+min(minDepth(root->left),minDepth(root->right));
+        int lh = minDepth(root->left);
+        int rh = minDepth(root->right);
+        if(!root->left) return 1 + rh;
+        if(!root->right) return 1 + lh;
+        return 1 + min(lh, rh);
     }
 };
