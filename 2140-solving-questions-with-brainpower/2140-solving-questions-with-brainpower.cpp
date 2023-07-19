20edf51088af37fe11cc266e524f1af5ec2e8c9a
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-07-19 18:30:13 +0530

    Time: 394 ms (68.50%), Space: 121.4 MB (51.44%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -5,6 +5,7 @@ public:
     long long f(int idx, vector<vector<int>>& q){
         if(idx >= q.size()) return 0;
         if(dp[idx] != -1) return dp[idx];
+        
         long long pick = 0, npick = 0;
         pick = q[idx][0] + f(idx + q[idx][1] + 1, q);
         npick = f(idx+1, q);
@@ -14,11 +15,13 @@ public:
     long long mostPoints(vector<vector<int>>& q) {
         int n = q.size();
         dp.resize(n, -1);
-        f(0, q);
+        return f(0, q);
+        /*
         long long ans = -1;
         for(int i = 0; i < n; i++){
             ans = max(ans, dp[i]);
         }
         return ans;
+        */
     }
 };
