12b1ca99d0710f7550683103ced22062b9a7beec
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-08-06 13:27:12 +0530

    Time: 404 ms (14.29%), Space: 130.5 MB (7.14%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int dr[4] = {0, -1, 1, 0};
+    int dc[4] = {-1, 0, 0, 1};
+    
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        queue<tuple<int, int, int>> q;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    q.push({i, j, 0});
+                    grid[i][j] = 0;
+                    vis[i][j] = 1;
+                } 
+            }
+        }
+
+        while(!q.empty()){
+            auto [r, c, v] = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i], nc = c + dc[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n && vis[nr][nc] == 0){
+                    vis[nr][nc] = 1;
+                    grid[nr][nc] = v+1;
+                    q.push({nr, nc, v+1});
+                }
+            }
+        }
+        
+        vector<vector<int>> dis(n, vector<int>(n, -1));
+        priority_queue<pair<int, pair<int, int>>> pq;
+        pq.push({grid[0][0], {0, 0}});
+        dis[0][0] = grid[0][0];
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int r = pq.top().second.first, c = pq.top().second.second;
+            pq.pop();
+            if(r == n-1 && c == n-1) return d;
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i], nc = c + dc[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n && min(d, grid[nr][nc]) > dis[nr][nc] ){
+                    dis[nr][nc] = min(d, grid[nr][nc]);
+                    pq.push({min(grid[nr][nc], d), {nr, nc}});
+                }
+            }
+        }
+        return 0;
+    }
+};
