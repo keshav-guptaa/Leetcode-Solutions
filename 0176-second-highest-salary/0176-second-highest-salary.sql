ad43b4e39b4bccadb1ca92a5ae94ec6e59ad22f8
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-08-30 18:45:38 +0530

    Time: 626 ms (19.34%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,4 @@
-SELECT max(Salary) AS SecondHighestSalary
-FROM Employee
-WHERE Salary < (SELECT max(Salary) FROM Employee)
+SELECT IFNULL((SELECT DISTINCT Salary
+	             FROM Employee 
+				 ORDER BY Salary DESC 
+				 LIMIT 1,1),NULL) AS SecondHighestSalary
