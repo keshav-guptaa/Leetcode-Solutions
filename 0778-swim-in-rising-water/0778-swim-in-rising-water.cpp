ee0062d242a069221595e60e9430e9dc53b3df60
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-12-19 12:26:28 +0530

    Time: 17 ms (57.36%), Space: 9.1 MB (84.51%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,27 +1,28 @@
+#define ti tuple<int, int, int>
 class Solution {
 public:
+    
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> time(n, vector<int>(n, 1e9));
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        int n = grid.size(), m = grid[0].size();
+        int dr[4] = {-1, 0, 0, 1}, dc[4] = {0, -1, 1, 0};
+        
+        vector<vector<int>> dis(n, vector<int>(m, 1e9));
+        priority_queue<ti, vector<ti>, greater<ti>> pq; 
         pq.push({grid[0][0], 0, 0});
-        time[0][0] = grid[0][0];
-        int dr[] = {-1, 0, 0, 1};
-        int dc[] = {0, 1, -1, 0};
+        dis[0][0] = grid[0][0];
         
         while(!pq.empty()){
-            auto [t, r, c] = pq.top();
+            auto [d, r, c] = pq.top();
             pq.pop();
-            if(r == n-1 && c == n-1) return t;
+            if(r == n-1 && c == m-1) return d;
             
             for(int i = 0; i < 4; i++){
                 int nr = r + dr[i], nc = c + dc[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
-                    if(max(grid[nr][nc], t) < time[nr][nc]){
-                        time[nr][nc] = max(grid[nr][nc], t);
-                        pq.push({time[nr][nc], nr, nc});
-                    }
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && dis[nr][nc] > max(d, grid[nr]                    [nc])){
+                    dis[nr][nc] = max(d, grid[nr][nc]);
+                    pq.push({dis[nr][nc], nr, nc});
                 }
+                    
             }
         }
         return -1;
