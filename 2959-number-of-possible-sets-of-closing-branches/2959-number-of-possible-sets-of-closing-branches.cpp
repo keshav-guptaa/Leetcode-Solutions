849202f06fed17be0973f5abb96d6feed6414963
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-12-19 20:41:54 +0530

    Time: 123 ms (87.06%), Space: 49 MB (74.07%) - LeetHub

--- 2959-number-of-possible-sets-of-closing-branches/2959-number-of-possible-sets-of-closing-branches.cpp
+++ 2959-number-of-possible-sets-of-closing-branches/2959-number-of-possible-sets-of-closing-branches.cpp
@@ -41,33 +41,4 @@ public:
         }
         return ans;
     }
-    /*
-    int getMaxDistance(int mask, int n, vector<vector<int>> d ) {
-    int res = 0;
-    for (int k = 0; k < n; ++k) // Floyd-Warshall
-        if (mask & (1 << k))
-            for (int i = 0; i < n; ++i)
-                if (i != k && mask & (1 << i))
-                    for (int j = 0; j < n; ++j)
-                        if (j != i && mask & (1 << j))
-                            d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
-    for (int i = 0; i < n; ++i)
-        if (mask & (1 << i))
-            for (int j = i + 1; j < n; ++j)
-                if (mask & (1 << j))
-                    res = max(res, d[i][j]);
-    return res;
-}
-int numberOfSets(int n, int maxDistance, vector<vector<int>>& roads) {
-    vector<vector<int>> d(n, vector<int>(n, 100000));
-    for (auto &r : roads) {
-        d[r[0]][r[1]] = min(d[r[0]][r[1]], r[2]);
-        d[r[1]][r[0]] = min(d[r[1]][r[0]], r[2]);
-    }
-    int comb = 1 << n, res = 1;
-    for (int mask = 1; mask < comb; ++mask)
-        res += getMaxDistance(mask, n, d) <= maxDistance;
-    return res;
-}
-    */
 };
