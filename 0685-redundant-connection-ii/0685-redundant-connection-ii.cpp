8b6961cd4825dc85ffbb58d07538da653806d8e0
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-12-23 18:13:26 +0530

    Time: 10 ms (37.19%), Space: 9.8 MB (77.00%) - LeetHub

--- 0685-redundant-connection-ii/0685-redundant-connection-ii.cpp
+++ 0685-redundant-connection-ii/0685-redundant-connection-ii.cpp
@@ -33,17 +33,17 @@ public:
     //1) Root got extra edge and has one parent now
     //2) Some other node got extra edge and has two parent now.
     // In first case, it will cause formation of cycle . Thus we return the edge causing cycle 
-    // In second case, it may or may not create a cycle . But we only have to check those 2            edges that leads to increase in indegree. For this we first remove the later edge and          check if a cycle is present or not . If cyle is present this is nnot the correct edge          else its the correct edge .
+    // In second case, it may or may not create a cycle . But we only have to check those 2            edges that leads to increase in indegree. For this we first remove the later edge and          check if a cycle is present or not . If cyle is present this is nnot the correct edge (1st edge is answer) else its the correct edge (2nd edge is answer).
     
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& e) {
         int n = e.size();
         DSU ds(n+1);
-        vector<int> in(n+1, -1);
+        vector<int> in(n+1, -1); //store index of edge
         int e1 = -1, e2 = -1;
         for(int i = 0; i < n; i++){
             if(in[e[i][1]] != -1){
-                e1 = in[e[i][1]];
-                e2 = i;
+                e1 = in[e[i][1]]; // first edge idx
+                e2 = i; // second edge idx
                 break;
             }
             in[e[i][1]] = i;
@@ -52,13 +52,11 @@ public:
         for(int i = 0; i < n; i++){
             if(i == e2) continue;   //We are actually ignoring later edge.
             if(ds.isConnected(e[i][0], e[i][1])){
-                if(e1 != -1){
-                    return e[e1];
-                }
-                else return e[i];
+                if(e1 != -1) return e[e1]; //Case 2(a)
+                else return e[i]; // Case 1
             }
-            ds.merge(e[i][0], e[i][1]);
-        }
-        return e[e2];
+            ds.merge(e[i][0], e[i][1]); 
+        } 
+        return e[e2]; //Case 2(b)
     }
 };
