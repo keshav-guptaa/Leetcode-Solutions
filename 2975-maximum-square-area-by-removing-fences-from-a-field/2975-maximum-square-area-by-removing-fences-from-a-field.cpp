7f3ab0d598c3b6a52794002d50ce22b10f2b8022
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-12-24 12:41:23 +0530

    Time: 1137 ms (100.00%), Space: 262.7 MB (100.00%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
 int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
-    long long ans = 0, mod= 1e9 + 7;
+    long long ans = -1, mod= 1e9 + 7;
     unordered_set<int> st;
     hFences.push_back(1); hFences.push_back(m); 
     vFences.push_back(1); vFences.push_back(n); 
-    for(int i = 0; i < hFences.size(); ++i){
-        for(int j = 0; j < hFences.size(); ++j){
-            if(i != j ) st.insert(abs(hFences[i] - hFences[j]));
+    for(int i = 0; i < hFences.size(); i++){
+        for(int j = i+1; j < hFences.size(); j++){
+            st.insert(abs(hFences[i]-hFences[j]));
         }
     }
-    for(int i = 0; i < vFences.size(); ++i){
-        for(int j = 0; j < vFences.size(); ++j){
-            if(i != j  && st.find(abs(vFences[i] - vFences[j])) != st.end()) {
-                ans = max(ans, (long long)abs(vFences[i] - vFences[j]));
+    for(int i = 0; i < vFences.size(); i++){
+        for(int j = i+1; j < vFences.size(); j++){
+            long long diff = abs(vFences[i]-vFences[j]);
+            if(st.find(diff) != st.end()){
+                ans = max(ans, diff);
             }
         }
     }
-    return (ans == 0)?-1: ((ans * ans) % mod);
-
+    return ans == -1 ? ans : (ans*ans)%mod;
 }
 };
