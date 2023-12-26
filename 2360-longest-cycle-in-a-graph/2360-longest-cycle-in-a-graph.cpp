3b75bea3ddd7688f481bfbf3042479978b04c891
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-12-26 17:49:51 +0530

    Time: 442 ms (17.30%), Space: 229.2 MB (13.25%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& edges, vector<int>& dist, vector<bool>& vis, vector<bool>& pathvis, int &ans, int distance){
-        if(node != -1){
-            if(!vis[node]){
-                vis[node] = 1;
-                pathvis[node] = 1;
-                dist[node] = distance;
-                dfs(edges[node], edges, dist, vis, pathvis, ans, distance+1);
-            }
-            else if(pathvis[node]){
-                ans = max(ans, distance - dist[node]);
-            }
-            pathvis[node] = 0;
+    int answer = -1;
+    void dfs(int node, vector<int>& edges, unordered_map<int, int>& dist, vector<bool>& visit) {
+        visit[node] = true;
+        int neighbor = edges[node];
+
+        if (neighbor != -1 && !visit[neighbor]) {
+            dist[neighbor] = dist[node] + 1;
+            dfs(neighbor, edges, dist, visit);
+        } else if (neighbor != -1 && dist.count(neighbor)) {
+            answer = max(answer, dist[node] - dist[neighbor] + 1);
         }
     }
-    
+
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<bool> vis(n, 0);
-        vector<bool> pathvis(n, 0);
-        vector<int> dist(n, 0);
-        int ans = -1;
-        
-        for(int i = 0; i < n; i++){
-            if(!vis[i]) dfs(i, edges, dist, vis, pathvis, ans, 0);
+        vector<bool> visit(n);
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                unordered_map<int, int> dist;
+                dist[i] = 1;
+                dfs(i, edges, dist, visit);
+            }
         }
-        return ans;
+        return answer;
     }
 };
