903753ed04c67f8d674f14da1d66bca6917b5066
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-12-29 14:32:58 +0530

    Time: 31 ms (95.06%), Space: 38.8 MB (81.75%) - LeetHub

---
+++ 2646-minimize-the-total-price-of-the-trips/2646-minimize-the-total-price-of-the-trips.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> adj;
+    int freq[100];
+    
+    bool dfs(int node, int par, int end){
+        if(node == end){
+            freq[node]++;
+            return 1;
+        }
+        
+        for(auto &it: adj[node]){
+            if(it == par) continue;
+            freq[node]++;
+            if(dfs(it, node, end)) return 1;
+            freq[node]--;
+        }
+        return 0;
+    }
+    
+    pair<int, int> findPrice(int node, int par, vector<int>& p){
+        int half = (p[node]/2)*freq[node];
+        int nothalf = p[node]*freq[node];
+        
+        for(auto &it: adj[node]){
+            if(it == par) continue;
+            pair<int, int> childCost = findPrice(it, node, p);
+            half += childCost.second;
+            nothalf += min(childCost.first, childCost.second);
+        }
+        return {half, nothalf};
+    }
+    
+    int minimumTotalPrice(int n, vector<vector<int>>& edges, vector<int>& price, vector<vector<int>>& trips) {
+        adj.resize(n);
+        for(auto &it: edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        //To count the occurences of each node in trips
+        for(auto &it: trips){
+            int st = it[0], dst = it[1];
+            dfs(st, -1, dst);
+        }
+        
+        pair<int, int> ans = findPrice(0, -1, price);
+        return min(ans.first, ans.second);
+    }
+};
