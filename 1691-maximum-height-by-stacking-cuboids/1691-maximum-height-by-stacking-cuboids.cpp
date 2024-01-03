a83d5848e38fd93a47096e5c1a99e914293f4548
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2024-01-03 20:20:01 +0530

    Time: 11 ms (76.15%), Space: 9.6 MB (60.92%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -5,16 +5,16 @@ public:
         int n = cub.size();
         for(auto &x: cub) sort(x.begin(), x.end());
         sort(cub.begin(), cub.end());
-        vector<int> h(n);
+        vector<int> dp(n);
         int mx = INT_MIN;
         for(int i = 0; i < n; i++){
-            h[i] = cub[i][2];
+            dp[i] = cub[i][2];
             for(int j = 0; j < i; j++){
                 if(cub[j][0] <= cub[i][0] && cub[j][1] <= cub[i][1] && cub[j][2] <= cub[i][2]){
-                    if(h[j]+cub[i][2] > h[i]) h[i] = h[j] + cub[i][2];
+                    if(dp[j]+cub[i][2] > dp[i]) dp[i] = dp[j] + cub[i][2];
                 }
             }
-            mx = max(mx, h[i]);
+            mx = max(mx, dp[i]);
         }
         return mx;
     }
