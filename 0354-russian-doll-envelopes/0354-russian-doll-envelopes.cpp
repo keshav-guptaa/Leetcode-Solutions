b7a3c45010c58160ac2afd5ee4505717df45864e
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2024-01-03 20:03:37 +0530

    Time: 275 ms (81.40%), Space: 77.7 MB (94.45%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& a, vector<int>& b){
-        if(a[0]==b[0]) return a[1] > b[1];
-        return a[0] < b[0];
-    }
-    
-    int maxEnvelopes(vector<vector<int>>& env) {
-        int n = env.size();
+    int maxEnvelopes(vector<vector<int>>& e) {
+        int n = e.size();
         vector<int> dp;
-        //dp.push_back(env[0][1]);
-        sort(env.begin(), env.end(), cmp);
+        sort(e.begin(), e.end(), [](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        
         for(int i = 0; i < n; i++){
-            
-            int idx = lower_bound(dp.begin(), dp.end(), env[i][1]) - dp.begin();
-            if(idx >= dp.size()) dp.push_back(env[i][1]);
-            else dp[idx] = env[i][1];
+            int idx = lower_bound(dp.begin(), dp.end(), e[i][1]) - dp.begin();
+            if(idx >= dp.size()) dp.push_back(e[i][1]);
+            else dp[idx] = e[i][1];
             
         }
         return dp.size();
