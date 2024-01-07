71eceff15b32c88393457d2522fa41a96548b9ea
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2024-01-07 13:43:43 +0530

    Time: 105 ms (76.32%), Space: 7.4 MB (77.19%) - LeetHub

--- 1617-count-subtrees-with-max-distance-between-cities/1617-count-subtrees-with-max-distance-between-cities.cpp
+++ 1617-count-subtrees-with-max-distance-between-cities/1617-count-subtrees-with-max-distance-between-cities.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    vector<int> countSubgraphsForEachDiameter(int n, vector<vector<int>>& edges) {
-        int INF = n; // Since cities form a tree so maximum distance between 2 cities always < n
-        vector<vector<int>> dist(n, vector<int>(n, INF));
-        for (auto& e : edges) 
-            dist[e[0]-1][e[1]-1] = dist[e[1]-1][e[0]-1] = 1;
-        
-        for (int k = 0; k < n; k++)
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-        
-        vector<int> ans(n - 1, 0);
-        for (int state = 0; state < (1 << n); state++) {
-            int d = maxDistance(state, dist, n);
-            if (d > 0) ans[d - 1] += 1;
-        }
-        return ans;
-    }
-    
-    int maxDistance(int state, vector<vector<int>>& dist, int n) {
-        int cntEdge = 0, cntCity = 0, maxDist = 0;
-        for (int i = 0; i < n; i++) {
-            if (((state >> i) & 1) == 0) continue; // Skip if city `i` not in our subset
-            cntCity += 1;
-            for (int j = i + 1; j < n; j++) {
-                if (((state >> j) & 1) == 0) continue; // Skip if city `j` not in our subset
-                cntEdge += dist[i][j] == 1;
-                maxDist = max(maxDist, dist[i][j]);
+    int findMaxDistance(int mask, vector<vector<int>>& dis, int n){
+        int edges = 0, nodes = 0, mx = 0;
+        for(int i = 0; i < n; i++){
+            if(mask & (1 << i)){
+                nodes++;
+                for(int j = i+1; j < n; j++){
+                    if(mask & (1 << j)){
+                        edges += (dis[i][j] == 1);
+                        mx = max(mx, dis[i][j]);
+                    }
+                }
             }
         }
-        if (cntEdge != cntCity - 1) return 0; // Subset form an invalid subtree!
-        return maxDist;
+        
+        if(edges == nodes-1) return mx;
+        return -1;
+    }
+    
+    vector<int> countSubgraphsForEachDiameter(int n, vector<vector<int>>& e) {
+        vector<vector<int>> dis(n, vector<int>(n, n+1));
+        for(auto &it: e){
+            dis[it[0]-1][it[1]-1] = dis[it[1]-1][it[0]-1] = 1;
+        } 
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+                }
+            }
+        }
+        
+        vector<int> ans(n-1, 0);
+        for(int mask = 0; mask < (1 << n); mask++){
+            int d = findMaxDistance(mask, dis, n);
+            if(d > 0) ans[d-1]++;
+        }
+        return ans;
     }
 };
