a8e61de89584c1f3ff1488c52fb5c5aaeb4c9c78
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-01-09 16:55:00 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (92.17%) - LeetHub

---
+++ 1643-kth-smallest-instructions/1643-kth-smallest-instructions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long nCr(int n, int r){
+        long long ans = 1;
+        for(int i = 1, j = n; i <= min(n-r, r); i++, j--) ans = (ans*j)/i;
+        return ans;
+    }
+    
+    string kthSmallestPath(vector<int>& d, int k) {
+        int h = d[1], v = d[0], tot = h+v;
+        string ans = "";
+        for(int i = 0; i < tot; i++){
+            if(h){
+                long long c = nCr(h+v-1, v);
+                if(k <= c){
+                    ans += 'H';
+                    h--;
+                }
+                else{
+                    ans += 'V';
+                    v--;
+                    k -= c;
+                }
+            }
+            else{
+                ans += 'V';
+                v--;
+            }
+        }
+        return ans;
+    }
+};
