7803dee6eed9fbcc8c43eb7fbeda46844306804d
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-01-10 21:47:47 +0530

    Time: 185 ms (48.29%), Space: 23.2 MB (54.79%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -6,11 +6,13 @@ public:
         sort(p.begin(), p.end());
         vector<int> dp(n, 1);
         
+        int ans = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < i; j++){
-                dp[i] = max(dp[i], p[i][0] > p[j][1] ? dp[j] + 1 : dp[j]);
+                if(p[i][0] > p[j][1] && 1 + dp[j] > dp[i]) dp[i] = 1 + dp[j];
             }
+            ans = max(ans, dp[i]);
         }
-        return dp[n-1];
+        return ans;
     }
 };
