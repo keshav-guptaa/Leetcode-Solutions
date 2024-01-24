7ddf94ce24e8410125b07fba811b0aacb58910c1
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-01-24 14:09:30 +0530

    Time: 7 ms (68.49%), Space: 11.6 MB (6.74%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -13,9 +13,13 @@ public:
         return lps;
     }
     
+    
+    //In other words, the problem statement is find the longest palindrome substring starting from index zero, then, the remaining string, we have to reverse and add it in starting.
     string shortestPalindrome(string s) {
         string r_s = s;
         reverse(r_s.begin(), r_s.end());
+        //How this temp string help us to find longest palindrome substring,
+        //"#" is reseting len = 0 in lps, now think in this way, in lps, at last index, there is last character of r_s, which is first character of s, so whatever stored here is telling us longest suffix == longest prefix in temp, which is nothing but longest palindrome substring starting from index zero, try to make lps array for s = "abaceba" on copy.
         string temp = s + "#" + r_s;
         vector<int> lps = calcLPS(temp);
         
