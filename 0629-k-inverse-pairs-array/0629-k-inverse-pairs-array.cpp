d6e0f2e4fb83d8de51dd557231131d54ce186a11
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-01-27 14:39:22 +0530

    Time: 2291 ms (7.69%), Space: 17.7 MB (10.99%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    const int mod = 1e9+7;
-    int kInversePairs(int n, int k) {
-        vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
-        dp[0][0] = 1;
+    int mod = 1e9+7;
+    
+    int f(int n, int k, vector<vector<int>>& dp){
+        if(k <= 0) return k == 0;
+        if(dp[n][k] != -1) return dp[n][k];
         
-        for(int i = 1; i <= n; i++){
-            for(int j = 0; j <= k; j++){
-                for(int l = 0; l <= min(j, i-1); l++){
-                    dp[i][j] = (dp[i][j] + dp[i-1][j-l])%mod;
-                }
-            }
-        }
-        return dp[n][k];
+        int ways = 0;
+        for(int i = 0; i <= k && i < n; i++){
+            ways = (ways + f(n-1, k-i, dp)) % mod;
+        } 
+        return dp[n][k] = ways;
+    }
+    
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> dp(n+1, vector<int>(k+1, -1));
+        // dp[0][0] = 1;
+        
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 0; j <= k; j++){
+        //         for(int l = 0; l <= min(j, i-1); l++){
+        //             dp[i][j] = (dp[i][j] + dp[i-1][j-l])%mod;
+        //         }
+        //     }
+        // }
+        return f(n, k, dp);
     }
 };
