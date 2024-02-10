f2d068f16a15e397b6efe6957c7f8eaff5398526
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-02-10 10:43:10 +0530

    Time: 4 ms (76.20%), Space: 7.4 MB (78.14%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n = s.length();
-        int ans = 0;
+        int n = s.length(), ans = 0;
+        //Odd length string
         for(int i = 0; i < n; i++){
             int l = i, r = i;
             while(l >= 0 && r < n){
-                if(s[l] == s[r]){
-                    ans++;
-                }
+                if(s[l--] == s[r++]) ans++;
                 else break;
-                l--;
-                r++;
             }
         }
+        //Even length string
         for(int i = 0; i < n-1; i++){
             int l = i, r = i+1;
             while(l >= 0 && r < n){
-                if(s[l] == s[r]){
-                    ans++;
-                }
+                if(s[l--] == s[r++]) ans++;
                 else break;
-                l--;
-                r++;
             }
         }
         return ans;
