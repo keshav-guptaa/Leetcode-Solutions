41e5b21f2be2989a36af807906166461aeb5248e
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-02-23 15:59:39 +0530

    Time: 7 ms (70.23%), Space: 9.2 MB (86.03%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,30 +1,17 @@
+typedef long long ll;
 class Solution {
 public:
-    vector<int> calcLPS(string& pattern){
-        int n = pattern.length(), i = 1, len = 0;
-        vector<int> lps(n, 0);
-        while(i < n){
-            if(pattern[i] == pattern[len]) lps[i++] = ++len;
-            else{
-                if(len != 0) len = lps[len-1];
-                else i++;
-            }
-        }
-        return lps;
-    }
-    
-    
-    //In other words, the problem statement is find the longest palindrome substring starting from index zero, then, the remaining string, we have to reverse and add it in starting.
     string shortestPalindrome(string s) {
-        string r_s = s;
-        reverse(r_s.begin(), r_s.end());
-        //How this temp string help us to find longest palindrome substring,
-        //"#" is reseting len = 0 in lps, now think in this way, in lps, at last index, there is last character of r_s, which is first character of s, so whatever stored here is telling us longest suffix == longest prefix in temp, which is nothing but longest palindrome substring starting from index zero, try to make lps array for s = "abaceba" on copy.
-        string temp = s + "#" + r_s;
-        vector<int> lps = calcLPS(temp);
-        
-        int idx = lps.back();
-        string extra = s.substr(idx);
+        int n = s.length();
+        int pos = -1, mod = 1e9+9, p = 31;
+        ll ph = 0, sh = 0, power = 1;
+        for(int i = 0; i < n; i++){
+            ph = (ph + (s[i]-'a'+1)*power)%mod;
+            power = (power*p)%mod;
+            sh = (sh*p + (s[i]-'a'+1))%mod;
+            if(ph == sh) pos = i;
+        }
+        string extra = s.substr(pos+1);
         reverse(extra.begin(), extra.end());
         return extra + s;
     }
