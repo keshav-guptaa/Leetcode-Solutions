7269d31785d794a7cc75af26c561ab9142ce923d
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-02-23 15:42:38 +0530

    Time: 147 ms (95.40%), Space: 68.3 MB (95.56%) - LeetHub

--- 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.cpp
+++ 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.cpp
@@ -13,10 +13,12 @@ public:
             int n = word.size();
             
             for(int i = 0, j = n - 1; i < n && j >= 0; i++, j--) {
+                //in prefix abc => 31^0 + 31^1 + 31^2, hence 'power' variable helps us.
                 prefix = (prefix + (word[i] - 'a' + 1) * power) % mod;
-                power = (power * p) % mod;
                 
+                //in suffix the order will be cab from last which is actually in string appear as abc, therefore, we want order 31^2 + 31^1 + 31^0, here, we use the concept of shifting (in decimal multiply by 10), therefore in base 31 (multiply by 31)
                 suffix = (suffix * p + (word[j] - 'a' + 1)) % mod;
+                power = (power * p) % mod;
                 
                 if(prefix == suffix && hash.find(prefix) != hash.end())
                     res += hash[prefix];
