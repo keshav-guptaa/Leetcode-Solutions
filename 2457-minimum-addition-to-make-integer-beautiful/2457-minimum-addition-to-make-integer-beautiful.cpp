7acbeccf3e6a011c0a405325fcc0cf25dc2dfb32
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-03-15 16:28:26 +0530

    Time: 3 ms (32.22%), Space: 7.1 MB (86.67%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int sumDigits(long long n) {
-    int res = 0;
-    for (; n; n /= 10)
-        res += n % 10;
-    return res;
-}
-long long makeIntegerBeautiful(long long n, int target) {
-    long long res = 0;
-    for (long long mul = 1; sumDigits(n) > target; mul *= 10) {
-        res += mul * (10 - n % 10);
-        n = n / 10 + 1;
+    long long makeIntegerBeautiful(long long n, int target) {
+        int sum = 0;
+        long long t = n, ans = 0;
+        while(t){
+            sum += t%10;
+            t /= 10;
+        }
+        
+        long long mul = 1, carry = 0;
+        
+        while(sum > target){
+            int digit = n%10 + carry;
+            if(digit != 0){
+                carry = 1;
+                ans += (10 - digit)*mul;
+                sum -= --digit;
+            }
+            else carry = 0;
+            n /= 10;
+            mul *= 10;
+        }
+        return ans;
     }
-    return res;
-}
 };
