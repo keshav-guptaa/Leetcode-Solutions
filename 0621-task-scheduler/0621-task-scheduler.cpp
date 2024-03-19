ed7fb4f9d8da88e211ff628d9539ab7c55948b0d
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-03-19 11:36:35 +0530

    Time: 85 ms (39.87%), Space: 38.2 MB (59.83%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> m;
+        map<char, int> m;
         for(auto &it: tasks) m[it]++;
-        priority_queue<int> pq;
-        for(auto &it: m) pq.push(it.second);
-        queue<pair<int, int>> q; //Task freq, time
-        int time = 1;
-        for(; ; time++){
-            if(!q.empty() && q.front().second <= time){
-                pq.push(q.front().first);
-                q.pop();
-            }
-            if(!pq.empty()){
-                int top = pq.top();
-                pq.pop();
-                --top;
-                if(top > 0) q.push({top, time + n + 1});
-            }
-            if(q.empty() && pq.empty()) break;
+        //time, char
+        priority_queue<pair<int, char>> pq;
+        for(char ch = 'A'; ch <= 'Z'; ch++){
+            if(m[ch] != 0) pq.push({0, ch});
         }
-        return time;
+        int ans = 0;
+        while(!pq.empty()){
+            auto [time, ch] = pq.top();
+            time = abs(time);
+            pq.pop();
+            m[ch]--;
+            if(time <= ans) ans++;
+            else ans = time+1;
+            if(m[ch] != 0) pq.push({-(time+n+1), ch});
+        }
+        return ans;
     }
 };
