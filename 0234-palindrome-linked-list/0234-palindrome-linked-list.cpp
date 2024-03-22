4a3980f1c6770b8ad77aa06a50cb62a4c6a8f20d
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-03-22 12:49:29 +0530

    Time: 160 ms (77.02%), Space: 116.6 MB (70.02%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,24 +11,28 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        //Find Middle of LL
         ListNode *slow = head, *fast = head;
         while(slow && fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
+        //Reverse LL
         ListNode *prev = NULL, *curr = slow, *ahead = NULL;
-        while(curr != NULL){
+        while(curr){
             ahead = curr->next;
             curr->next = prev;
             prev = curr;
             curr = ahead;
         }
+        //Two pointer approach to check palindrome
         ListNode *start = head, *end = prev;
         while(start && end){
             if(start->val != end->val) return false;
             start = start->next;
             end = end->next;
         }
+        
         return true;
     }
 };
