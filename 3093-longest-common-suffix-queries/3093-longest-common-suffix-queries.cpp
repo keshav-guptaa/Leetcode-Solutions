6b0e6608d10ea4f41197968328803bd7f94f2818
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-03-24 09:43:21 +0530

    Time: 422 ms (83.33%), Space: 823.4 MB (16.67%) - LeetHub

---
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -0,0 +1,61 @@
+struct TrieNode{
+  public:
+    int smallest = 1e4;
+    int idx = 1e6;
+    TrieNode* dict[26] = {};
+};
+
+class Solution {
+public:
+    TrieNode* root = new TrieNode();
+    
+    void insert(string s, int id){
+        TrieNode* node = root;
+        
+        
+        for(int i = 0; i < s.length(); i++){
+            if(node->dict[s[i]-'a'] == NULL){
+                node->dict[s[i]-'a'] = new TrieNode();
+            }
+            node = node->dict[s[i]-'a'];
+            if(s.length() < node->smallest){
+                node->smallest = s.length();
+                node->idx = id;
+            }
+            else if(s.length() == node->smallest) node->idx = min(node->idx, id);
+            
+        }
+        
+    }
+    
+    int startsWith(string s) {
+        TrieNode* node = root;
+        for(int i=0;i<s.length();i++){
+            if(node->dict[s[i]-'a'] == NULL) return node->idx;
+            node = node->dict[s[i]-'a'];
+        }
+        cout << node->smallest << endl;
+        return node->idx;
+    }
+    
+    vector<int> stringIndices(vector<string>& wc, vector<string>& wq) {
+        int idx = 1e6, smallest = 1e4;
+        for(int i = 0; i < wc.size(); i++){
+            reverse(wc[i].begin(), wc[i].end());
+            insert(wc[i], i);
+            if(wc[i].length() < smallest){
+                smallest = wc[i].length();
+                idx = i;
+            }
+            else if(wc[i].length() == smallest) idx = min(idx, i);
+        } 
+        root->idx = idx; 
+        int n = wq.size();
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            reverse(wq[i].begin(), wq[i].end());
+            ans[i] = startsWith(wq[i]);
+        }
+        return ans;
+    }
+};
