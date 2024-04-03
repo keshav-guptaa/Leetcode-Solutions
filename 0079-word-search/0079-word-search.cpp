f9f7a0def3583218e93d05a00177bccc94cbb4a4
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-04-03 18:00:45 +0530

    Time: 236 ms (79.69%), Space: 9.6 MB (78.94%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -5,12 +5,14 @@ public:
     
     bool f(int idx, int r, int c, vector<vector<char>>& b, string& word, int n, int m,
            vector<vector<int>>& vis){
-        if(idx == word.length()-1 && word[idx] == b[r][c]) return true;
+        if(!vis[r][c] && word[idx] == b[r][c]){
+            if(idx == word.length()-1) return true;
+        }
+        else return false;
         vis[r][c] = 1;
         for(int i = 0; i < 4; i++){
             int nr = r + dr[i], nc = c + dc[i];
-            if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] 
-               && word[idx+1] == b[nr][nc]){
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc]){
                 //cout << word[idx] << endl;
                 if(f(idx+1, nr, nc, b, word, n, m, vis)){
                     return true;
@@ -26,9 +28,7 @@ public:
         vector<vector<int>> vis(n, vector<int>(m, 0));
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(word[0] == b[i][j]){
-                    if(f(0, i, j, b, word, n, m, vis)) return true;
-                }
+                if(f(0, i, j, b, word, n, m, vis)) return true;
             }
         }
         return false;
