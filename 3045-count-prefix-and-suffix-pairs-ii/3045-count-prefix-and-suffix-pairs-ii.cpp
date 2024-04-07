e8aaf243cce8c5edf42d028b3ff903af9d7d43ef
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-04-07 13:28:36 +0530

    Time: 440 ms (66.20%), Space: 262.5 MB (46.82%) - LeetHub

--- 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.cpp
+++ 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.cpp
@@ -1,33 +1,31 @@
-typedef long long ll;
-class Solution { 
+struct TrieNode{
+  public:
+    map<pair<char, char>, TrieNode*> next;
+    long long ct = 0;
+};
+
+class Solution {
 public:
-    long long countPrefixSuffixPairs(vector<string>& words) {
-        const int p = 31, mod = 1e9 + 9;
-        ll res = 0;
-        unordered_map<ll, int> hash;
-        
-        for(string word : words) {
-            ll prefix = 0, suffix = 0;
-            ll power = 1;
-            
-            int n = word.size();
-            
-            for(int i = 0, j = n - 1; i < n && j >= 0; i++, j--) {
-                //in prefix abc => 31^0 + 31^1 + 31^2, hence 'power' variable helps us.
-                prefix = (prefix + (word[i] - 'a' + 1) * power) % mod;
-                
-                //in suffix the order will be cab from last which is actually in string appear as abc, therefore, we want order 31^2 + 31^1 + 31^0, here, we use the concept of shifting (in decimal multiply by 10), therefore in base 31 (multiply by 31)
-                suffix = (suffix * p + (word[j] - 'a' + 1)) % mod;
-                power = (power * p) % mod;
-                
-                if(prefix == suffix && hash.find(prefix) != hash.end())
-                    res += hash[prefix];
-            }
-            
-            hash[prefix]++;
-            // hash.insert(suffix);
+    TrieNode* root = new TrieNode();
+    
+    long long insert(string& s){
+        long long ans = 0;
+        TrieNode* node = root;
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            pair<char, char> key = {s[i], s[n-i-1]};
+            if(node->next[key] == NULL) node->next[key] = new TrieNode();
+            ans += node->ct;
+            node = node->next[key];
         }
-        
-        return res;
+        ans += node->ct;
+        node->ct++;
+        return ans;
+    }
+    
+    long long countPrefixSuffixPairs(vector<string>& words) {
+        long long ans = 0;
+        for(auto& it: words) ans += insert(it);
+        return ans;
     }
 };
