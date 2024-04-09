68919528c3a0b4fc943acf724ba65eccea4ee419
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-04-09 13:29:35 +0530

    Time: 781 ms (9.79%), Space: 172.2 MB (93.81%) - LeetHub

--- 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.cpp
+++ 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    vector<int> adj[100005];
+    vector<int> g[100005];
     int dp[100005][15];
     int k;
-
-    int dfs2(int src,int par,int times,vector<int> &coins){
-        int choice1 = 0, choice2 = 0;
-        if(dp[src][times]!=-1) return dp[src][times];
+    
+    int f(int node, int ops, int parent, vector<int>& c){
+        if(ops > 13) return 0;
+        if(dp[node][ops] != -1) return dp[node][ops];
         
-        for(auto nei:adj[src]){
-            if(par==nei) continue;
-            choice1 += dfs2(nei,src,min(14,times+1),coins);
-            choice2 += dfs2(nei,src,times,coins);
+        int p1 = (c[node] >> ops) - k;
+        int p2 = c[node] >> (ops+1);
+        for(auto& it: g[node]){
+            if(it == parent) continue;
+            p1 += f(it, ops, node, c);
+            p2 += f(it, ops+1, node, c);
         }
-        
-        int coinsAvailableAtSource = coins[src]/(1<<times);
-        choice1 += coinsAvailableAtSource/2;
-        choice2 += coinsAvailableAtSource-k;
-
-        return dp[src][times] = max(choice1, choice2);
+        return dp[node][ops] = max(p1, p2);
     }
     
-    
-    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int kk) {
-        k = kk;
-        memset(dp,-1,sizeof(dp));
-        for(auto e:edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+    int maximumPoints(vector<vector<int>>& e, vector<int>& c, int K) {
+        k = K;
+        int n = c.size();
+        memset(dp, -1, sizeof dp);
+        for(auto& it: e){
+            g[it[0]].push_back(it[1]);
+            g[it[1]].push_back(it[0]);
         }
-        return dfs2(0,-1,0,coins);   
+        return f(0, 0, -1, c);
     }
 };
