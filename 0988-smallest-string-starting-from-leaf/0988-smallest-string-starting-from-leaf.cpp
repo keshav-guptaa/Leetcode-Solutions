3ff28ad2bcaef86b026ac38ac8b3088524266aaa
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-04-17 18:47:27 +0530

    Time: 3 ms (98.00%), Space: 20.3 MB (78.11%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,9 +11,22 @@
  */
 class Solution {
 public:
-    string smallestFromLeaf(TreeNode* r, string s = "") {
-        s = string(1, 'a' + r->val) + s;
-        return r->left == r->right ? s : min(r->left ? smallestFromLeaf(r->left, s) : "|", 
-            r->right ? smallestFromLeaf(r->right, s) : "|");
+    string smallestString;
+    
+    string smallestFromLeaf(TreeNode* root) {
+        dfs(root, "");
+        return smallestString;
+    }
+
+    void dfs(TreeNode* root, string currentString) {
+        if (!root) return;
+        currentString = char(root->val + 'a') + currentString; 
+        if (!root->left && !root->right){ 
+            if (smallestString == "" || smallestString > currentString) { 
+                smallestString = currentString;
+            }
+        }
+        if (root->left) dfs(root->left, currentString);
+        if (root->right) dfs(root->right, currentString);
     }
 };
