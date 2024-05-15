daf654cfb336334c60ceab279a06bbe0d9a36b5a
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-05-15 10:47:39 +0530

    Time: 318 ms (97.44%), Space: 145.8 MB (70.93%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dr[4] = {0, -1, 1, 0};
-    int dc[4] = {-1, 0, 0, 1};
+    int dr[4] = {-1, 0, 1, 0};
+    int dc[4] = {0, -1, 0, 1};
     
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
