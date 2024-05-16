4c5b0c729b9c634e81a6d94145611112605cd2d7
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-05-16 16:00:59 +0530

    Time: 59 ms (80.15%), Space: 43.2 MB (87.50%) - LeetHub

--- 2589-minimum-time-to-complete-all-tasks/2589-minimum-time-to-complete-all-tasks.cpp
+++ 2589-minimum-time-to-complete-all-tasks/2589-minimum-time-to-complete-all-tasks.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int findMinimumTime(vector<vector<int>>& tasks) {
         sort(tasks.begin(),tasks.end(),[](vector<int>& a,vector<int>& b){
-            return (a[1]!=b[1])?a[1]<b[1]:a[0]<b[0];
+            return a[1] < b[1];
         });
-        unordered_map<int,bool> used;       // time_slice... used or not ... 
         
-        int count=0;
-        for(auto& task:tasks){
-            int usedTime=0;
-            for(int t=task[0];t<=task[1];t++)   if(used.count(t))   usedTime++;      // have used this time slice
-
-            for(int t=task[1];usedTime<task[2];t--){
-                if(!used.count(t)){         // this time slice is available
-                    usedTime++;             // duration for which I run it
-                    used[t]=1;              // use this timeslice
-                    count++;                // time for which my computer is on 
-                }
+        bool used[2005] = {};       
+        int count = 0;
+        for(auto& task: tasks){
+            int start = task[0], end = task[1], duration = task[2];
+            //If some time of current interval used in previous task then current task can also be done in this interval because computer can do unlimited tasks at the same time and it also minimizes the time for which computer is on.
+            for(int i = start; i <= end; i++){
+                if(used[i]) duration--;
+            }
+            //I will try to complete the task at the end of allowed range because it may overlap with some other tasks starting range and hence reduces our time for which computer is on.
+            for(int i = end; duration > 0; i--){
+                if(used[i]) continue;
+                duration--;
+                used[i] = true;
+                count++;
             }
         }
         return count;
