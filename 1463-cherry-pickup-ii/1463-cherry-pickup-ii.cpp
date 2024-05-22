612881b8a9bcc1c7f620873d14f74eceb1aadd30
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-05-22 17:37:06 +0530

    Time: 24 ms (95.45%), Space: 12.6 MB (78.10%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,16 +3,14 @@ public:
     int dp[72][72][72], n, m;
     
     int f(int r, int c1, int c2, vector<vector<int>>& grid){
-        if(r == n) return 0;
+        if(r == n || c1 < 0 || c2 < 0 || c1 >= m || c2 >= m) return 0;
         if(dp[r][c1][c2] != -1) return dp[r][c1][c2];
         
         int ans = 0;
         for(int i = -1; i <= 1; i++){
             for(int j = -1; j <= 1; j++){
                 int nc1 = c1 + i, nc2 = c2 + j;
-                if(nc1 >= 0 && nc1 < m && nc2 >= 0 && nc2 < m){
-                    ans = max(ans, f(r+1, nc1, nc2, grid));
-                }
+                ans = max(ans, f(r+1, nc1, nc2, grid));
             }
         }
         ans += (c1 == c2) ? grid[r][c1] : grid[r][c1] + grid[r][c2];
