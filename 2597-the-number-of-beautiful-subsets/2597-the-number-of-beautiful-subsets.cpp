a438ebfac4039c20ca20dcf762c0f6a0f7ddc2c7
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-05-23 19:32:31 +0530

    Time: 775 ms (19.22%), Space: 40.3 MB (16.94%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    
-        
-    int ans=0;
-    void recur(vector<int>&curr,vector<int>&nums,int k,int ind){
-        if(ind==nums.size()){
-            if(curr.size())ans++;
-            return;
+    int dfs(vector<int> &nums, int idx, int k, unordered_map<int, int> &mp) {
+        if(idx == nums.size()) return 1;
+
+        int taken = 0;
+        if(!mp[nums[idx] - k] && !mp[nums[idx] + k]) {
+            mp[nums[idx]]++;
+            taken = dfs(nums, idx + 1, k, mp);
+            mp[nums[idx]]--;
         }
-        if(binary_search(curr.begin(),curr.end(),nums[ind]-k)){
-            recur(curr,nums,k,ind+1);
-            return;
-        }
-        recur(curr,nums,k,ind+1);
-        curr.push_back(nums[ind]);
-        recur(curr,nums,k,ind+1);
-        curr.pop_back();
+
+        int notTaken = dfs(nums, idx + 1, k, mp);
+
+        return taken + notTaken;
     }
-    
-    
-    int beautifulSubsets(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        vector<int>curr;
-        recur(curr,nums,k,0);
-        return ans;
+
+    int beautifulSubsets(vector<int> &nums, int k) {
+        unordered_map<int, int> mp;
+        int ans = dfs(nums, 0, k, mp);
+        return ans - 1;
     }
-        
-        
-    
 };
