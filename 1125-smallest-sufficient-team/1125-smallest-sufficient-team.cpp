02be770fba041a57f24badba7582b887c9a61515
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-05-27 11:40:02 +0530

    Time: 80 ms (46.99%), Space: 24.8 MB (64.59%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
         int n = req_skills.size();
         unordered_map<int, vector<int>> dp;
-        dp.reserve(1 << n); 
-        dp[0] = {};
+        dp.reserve(1 << n); // reserve space and avoid hash collisions
+        dp[0] = {}; //necessary for initial interation of dp map
         unordered_map<string, int> skills_to_idx;
         for(int i = 0; i < n; i++) skills_to_idx[req_skills[i]] = i;
         
@@ -12,10 +12,10 @@ public:
             int curr_skill = 0;
             for(auto &it: people[i]) curr_skill |= (1 << skills_to_idx[it]);
             for(auto it = dp.begin(); it != dp.end(); it++){
-                int mask = curr_skill | it->first;
-                if(dp.count(mask) == 0 || dp[mask].size() > 1 + dp[it->first].size()){
-                    dp[mask] = it->second;
-                    dp[mask].push_back(i);
+                int mask = it->first, nmask = curr_skill | mask;
+                if(dp.count(nmask) == 0 || dp[nmask].size() > 1 + dp[mask].size()){
+                    dp[nmask] = it->second; // put old mask elements
+                    dp[nmask].push_back(i); // put current element
                 }
             }
         }
