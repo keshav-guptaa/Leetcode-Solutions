8b7e125e48da53ce1fbb5e2cfc92abbf1621dfee
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-05-28 11:35:39 +0530

    Time: 9 ms (61.32%), Space: 10.6 MB (94.29%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -7,11 +7,10 @@ public:
         if(idx == n) return 0;
         if(dp[idx] != -1) return dp[idx];
         
-        int mx = INT_MIN, ans = INT_MIN, len = 0;
+        int mx = INT_MIN, ans = INT_MIN;
         for(int j = idx; j < min(n,idx+k); j++){
-            len++;
             mx = max(mx, arr[j]);
-            int sum = len*mx + f(j+1, arr, k);
+            int sum = (j-idx+1)*mx + f(j+1, arr, k);
             ans = max(ans, sum);
         }
         return dp[idx] = ans;
