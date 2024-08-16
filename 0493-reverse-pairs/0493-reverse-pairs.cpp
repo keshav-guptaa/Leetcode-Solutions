d3d688a97a61ab49136cbaf5380169dabae0910c
Keshav Gupta <97406076+kshavgupta@users.noreply.github.com>
2024-08-16 18:20:48 +0530

    Time: 526 ms (34.16%), Space: 91.7 MB (90.49%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -2,19 +2,19 @@
 using namespace __gnu_pbds;
     
 template<typename T>
-using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 class Solution {
 public:
     int reversePairs(vector<int>& nums) {
         int n = nums.size();
-        ordered_set<long long>s;
+        ordered_set<pair<long long, int>>s;
         for(int i = 0; i < n; ++i){
-            s.insert(nums[i]);
+            s.insert({nums[i], i});
         }
         long long ans = 0;
-        for(int i = n-1; i >= 1; --i){
-            s.erase( s.upper_bound(nums[i]) );
-            ans += s.size() - s.order_of_key(2*1LL*nums[i] + 1);
+        for(int i = n-1; i >= 0; --i){
+            s.erase({nums[i], i});
+            ans += s.size() - s.order_of_key({2*1LL*nums[i] + 1, -1});
         }
         return ans;
     }
